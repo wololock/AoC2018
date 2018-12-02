16a0d51e7b2ba777c4292fd3ef9c7582f0028ad5
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-12-02 08:21:41 +0100

    Day 2: solution part 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,17 +1,19 @@
 module Day02 where
 
 import Commons
-import Data.List
+import Data.List (nub,sort,group,maximumBy)
+import Data.Function (on)
+import Control.Arrow ((***))
 
 parseInput :: String -> [String]
-parseInput = concat . map words . lines
+parseInput = concatMap words . lines
 
 
 checksum :: [String] -> Int
-checksum xss = fst tup * snd tup
+checksum xss = uncurry (*) tup
                where
                 yss = map checkCandidate xss
-                tup = foldl (\acc tup' -> (fst acc + fst tup', snd acc + snd tup')) (0,0) yss
+                tup = foldl (\acc tup' -> (((+) (fst acc) *** (+) (snd acc)) tup')) (0,0) yss
 
 
 checkCandidate :: String -> (Int,Int)
@@ -22,9 +24,24 @@ checkCandidate xs = (twos, threes)
                         threes = if 3 `elem` ys then 1 else 0                     
 
 
+commonElems :: Eq a => [a] -> [a] -> [a]
+commonElems xs ys = map fst (filter (uncurry (==)) $ zip xs ys)
+
+findCommons :: Eq a => [a] -> [[a]] -> [[a]]
+findCommons s = map (commonElems s)
+
+longest :: [[a]] -> [a]
+longest = maximumBy (compare `on` length)
+
+part2 :: [String] -> [String]
+part2 [x]    = [""]
+part2 (x:xs) = longest (findCommons x xs) : part2 xs
+
+
 solution :: IO ()
 solution = do putStr "Part 01: ";
               input <- parseInput <$> getInput "input_02.txt";
               print (checksum input)
               putStr "Part 02: "
-              print "..."
+              print (longest $ part2 input)
+              
