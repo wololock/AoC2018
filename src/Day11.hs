216697706f8a43c581403249d2fcd014458a5e56
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-12-11 11:32:25 +0100

    Day 11: solution part 2

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,34 +1,40 @@
 import Commons
-import Data.List (sortBy)
-import Data.Monoid ((<>))
+import Data.List (maximumBy)
+import Data.Array
 
 type Pos = (Int,Int)
 
 hundredDigit :: Int -> Int
 hundredDigit n | n < 1000  = n `div` 100
-               | otherwise = read ([head digits]) :: Int
+               | otherwise = read [head digits] :: Int
                              where
                                 n'     = show (n `mod` 1000)
-                                digits = (replicate (3 - (length n')) '0') ++ n'
+                                digits = replicate (3 - length n') '0' ++ n'
 
 powerLevel :: Pos -> Int -> Int
-powerLevel (x,y) serial = (hundredDigit (((rackId * y) + serial) * rackId)) - 5
+powerLevel (x,y) serial = hundredDigit (((rackId * y) + serial) * rackId) - 5
                           where
                              rackId = x + 10  
                           
 
-pos2square :: Pos -> [Pos]                             
-pos2square (x,y) = [(x-1,y-1),(x,y-1),(x+1,y-1),
-                    (x-1,y),  (x,y),  (x+1,y),
-                    (x-1,y+1),(x,y+1),(x+1,y+1)]
+pos2square :: Pos -> Int -> [Pos]                             
+pos2square (x,y) s = [(x',y') | x' <- [x..(x+s-1)], y' <- [y..(y+s-1)]]
 
 
-part01 :: Int -> (Int,Pos)
-part01 serial = head $ sortBy (\(n,(x,y)) (n',(x',y')) -> compare n' n <> compare x x' <> compare y y') $ [(sum $ map (\p -> powerLevel p serial) (pos2square (x,y)), (x-1,y-1)) | x <- [2..298], y <- [2..298]]
+part01 :: Int -> [Int] -> (Int, (Int,Pos))
+part01 serial xs = maximumBy (\(_,(p,_)) (_,(p',_)) -> compare p p') [(s, maximum [(sum $ map (\p -> grid ! p) $ pos2square (x,y) s, (x,y)) | x <- [1..(300-s+1)], y <- [1..(300-s+1)]]) | s <- xs]
+                   where
+                     grid = array ((1,1),(300,300)) [((x,y), powerLevel (x,y) serial) | x <- [1..300], y <- [1..300]]
+                   
 
 solution :: IO ()
 solution = do putStr "Part 01: "
-              print $ part01 2694
+              let serial = 2694
+              let (s,(p,(x,y))) = part01 serial [3]
+              print $ show x ++ "," ++ show y
+              putStr "Part 02: "
+              let (s',(p',(x',y'))) = part01 serial [3..24]
+              print $ show x' ++ "," ++ show y' ++ "," ++ show s'
 
 main :: IO ()
 main = solution
