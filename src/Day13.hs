5951a9a5e8fddaa5348253f356b9f0c41f0fdfb2
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-12-13 23:47:22 +0100

    Day 13: solution part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,13 +1,18 @@
 import Commons
 import Parser
 import Data.Array
+import Data.Monoid ((<>))
+import Data.List (sortBy)
 
 type Pos = (Int,Int)
 type Track = Array Pos Char
-type Cart = (Pos,Direction,Int)
+type Cart = (Pos,Direction,Turn)
 
-data Direction = North | South | West | East
-                 deriving (Show,Eq)
+data Direction = North | South | East | West
+                 deriving (Eq,Ord,Show)
+
+data Turn = L | S | R
+            deriving (Eq,Ord,Show)
 
 char2direction :: Char -> Maybe Direction
 char2direction c | c == '>'   = Just East
@@ -15,25 +20,6 @@ char2direction c | c == '>'   = Just East
                  | c == 'v'   = Just South
                  | c == '^'   = Just North
                  | otherwise  = Nothing
-
--- left -> straight -> right
-turn :: Direction -> Int -> Direction
-turn South n | n `mod` 3 == 0 = East
-             | n `mod` 3 == 1 = South
-             | otherwise      = West
-
-turn North n | n `mod` 3 == 0 = West
-             | n `mod` 3 == 1 = North
-             | otherwise      = East
-
-turn East n  | n `mod` 3 == 0 = North
-             | n `mod` 3 == 1 = East
-             | otherwise      = South
-
-turn West n  | n `mod` 3 == 0 = South
-             | n `mod` 3 == 1 = West
-             | otherwise      = North
-
 
 parseCarts :: [String] -> [Cart]
 parseCarts = parseLine 0
@@ -45,7 +31,7 @@ parseCarts = parseLine 0
                                           process :: [Cart]
                                           process = foldl (\acc (m,c) -> case char2direction c of
                                                                           Nothing -> acc
-                                                                          Just d  -> ((m,n), d, 0) : acc
+                                                                          Just d  -> ((m,n), d, L) : acc
                                                     ) [] (zip [0..] xs)
 
 parseTrack :: String -> Track
@@ -62,6 +48,25 @@ parseTrack input = create
                        where c = input' !! i
 
 
+turn :: Direction -> Turn -> (Direction, Turn)
+turn North t = case t of
+                L -> (West, S)
+                S -> (North, R)
+                R -> (East, L)
+turn South t = case t of 
+                L -> (East, S)
+                S -> (South, R)
+                R -> (West, L)
+turn East t = case t of
+                L -> (North, S)
+                S -> (East, R)
+                R -> (South, L)
+turn West t = case t of 
+                L -> (South, S)
+                S -> (West, R)
+                R -> (North, L)
+
+
 nextPos :: Pos -> Direction -> Pos
 nextPos (x,y) North = (x, y-1)
 nextPos (x,y) South = (x, y+1)
@@ -70,42 +75,82 @@ nextPos (x,y) East  = (x+1, y)
 
 
 move :: Cart -> Track -> Cart
-move ((x,y), d, n) t = ((x',y'), d', n')
+move ((x,y), d, r) t = ((x',y'), d', r')
                        where
                          (x',y') = nextPos (x,y) d
                          c       = t ! (x',y')
-                         (d',n') = case c of
-                                    '-'  -> (d,n)
-                                    '|'  -> (d,n)
+                         (d',r') = case c of
+                                    '-'  -> (d,r)
+                                    '|'  -> (d,r)
                                     '\\' -> case d of
-                                             South -> (East,n)
-                                             North -> (West,n)
-                                             East  -> (South,n)
-                                             West  -> (North,n)
+                                             South -> (East,r)
+                                             North -> (West,r)
+                                             East  -> (South,r)
+                                             West  -> (North,r)
                                     '/'  -> case d of
-                                             South -> (West,n)
-                                             North -> (East,n)
-                                             East  -> (North,n)
-                                             West  -> (South,n)
-                                    '+'  -> (turn d n, n+1)
+                                             South -> (West,r)
+                                             North -> (East,r)
+                                             East  -> (North,r)
+                                             West  -> (South,r)
+                                    '+'  -> turn d r
 
 
 detectColisions:: [Cart] -> [Pos]
 detectColisions carts = fst (foldl (\(l,r) (p,_,_) -> if p `elem` r then (p:l, r) else (l, p:r)) ([],[]) carts)
 
-part01 :: [Cart] -> Track -> Pos
-part01 cs t = tick 0 cs
-              where
-                tick :: Int -> [Cart] -> Pos
-                tick n cs' = if null cols then tick (n+1) cs'' else head cols
-                             where
-                                cols = detectColisions cs'
-                                cs'' = map (`move` t) cs'
+part01 :: Track -> [Cart] -> Pos
+part01 t = tick 0
+           where
+             tick :: Int -> [Cart] -> Pos
+             tick n cs = if null cols then tick (n+1) cs' else head cols
+                         where
+                            (cs',cols) = makeMove (sortBy (\((x,y),_,_) ((x',y'),_,_) -> compare y y' <> compare x x') cs) ([],[])
+                            makeMove :: [Cart] -> ([Cart],[Pos])-> ([Cart],[Pos])
+                            makeMove [] acc           = acc                    
+                            makeMove (c:cs) (ns,cols) = makeMove cs' (ns',cols')
+                                                        where
+                                                          (p,d,i)     = move c t
+                                                          crash       = collision (p,d,i) (cs ++ ns)
+                                                          (ns',cols') | crash     = (filter (\(p',_,_) -> p /= p') ns, cols ++ [p])
+                                                                      | otherwise = (ns ++ [(p,d,i)], cols)
+                                                          cs'         | crash     = filter (\(p',_,_) -> p /= p') cs
+                                                                      | otherwise = cs
 
-  
+
+
+part02 :: Track -> [Cart] -> Pos
+part02 t = tick 0
+           where
+             tick :: Int -> [Cart] -> Pos
+             tick n cs = if length cs' <= 1 then pos (head cs') else tick (n+1) cs'
+                         where
+                            pos :: Cart -> Pos
+                            pos (p,d,n) = p
+                            cs'         = makeMove (sortBy (\((x,y),_,_) ((x',y'),_,_) -> compare y y' <> compare x x') cs) []
+                            makeMove :: [Cart] -> [Cart]-> [Cart]
+                            makeMove [] acc     = acc
+                            makeMove (c:cs) acc = makeMove cs' acc'
+                                                  where
+                                                     (p,d,i) = move c t
+                                                     crash   = collision (p,d,i) (cs ++ acc)
+                                                     acc'    | crash     = filter (\(p',_,_) -> p /= p') acc
+                                                             | otherwise = acc ++ [(p,d,i)]
+                                                     cs'     | crash     = filter (\(p',_,_) -> p /= p') cs
+                                                             | otherwise = cs
+
+                               
+collision :: Cart -> [Cart] -> Bool
+collision (p,_,_) cs = any (\(p',_,_) -> p == p') cs
+
+
 solution :: IO ()
-solution = do putStr "Part 01: "
-              input <- getInput "input_13.txt"
+solution = do input <- getInput "input_13.txt"
               let carts = parseCarts (lines input)
-              let track = parseTrack input            
-              print $ part01 carts track
+              let track = parseTrack input           
+              putStr "Part 01: "
+              print $ part01 track carts
+              putStr "Part 02: "
+              print $ part02 track carts
+              
+main :: IO ()
+main = solution
